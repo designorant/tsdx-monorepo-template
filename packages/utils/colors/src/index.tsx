4d13356aa4fb91147cdc0db21909ba1aa5a150e0
Michał Ordon <michal@tehidea.com>
2019-09-09 14:56:06 +0100

    add JSX component

--- packages/utils/colors/src/index.tsx
+++ packages/utils/colors/src/index.tsx
@@ -1,3 +1,7 @@
+import * as React from 'react';
+
+export const Foobar: React.FunctionComponent = () => <div>Foobar</div>;
+
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
     console.log('boop');
